42fab37c76ca389fef157b484560b0b25bb85ea1
Felipe_Mag3s <felipe@mag3s.com>
2021-11-29 09:57:47 -0300

    primeiro commit

---
+++ Palindromo/Palindromo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string ReverseString(string a)
+            {
+                char[] b = a.ToCharArray();
+                Array.Reverse(b);
+                return new string(b);
+            }
+
+            Console.WriteLine(ReverseString("Felipe"));
+
+            Console.WriteLine("Informe o nome");
+            string c = Console.ReadLine();
+
+            Console.WriteLine(ReverseString(c));
+        }
+    }
+}
